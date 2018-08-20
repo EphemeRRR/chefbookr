5277abbd8457755135c310af2a65d263675e8df2
Francois Marie Verdier <francoismarie.verdier@gmail.com>
2018-08-20 16:48:11 +0200

    add chef model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_20_143301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chefs", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "experience"
+    t.string "location"
+    t.string "photo_id"
+    t.string "email"
+    t.boolean "availability"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
