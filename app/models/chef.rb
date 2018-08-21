a2d900ea386d681bca6f587c686c9c673578a2bc
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-21 12:28:35 +0200

    add email and experience validations to Chef model

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,9 +2,11 @@ class Chef < ApplicationRecord
   has_many :specialties
   has_many :menus
   has_many :bookings, through: :menus, dependent: :destroy
+
+  validates :experience, numericality: { only_integer: true }
+  validates :email, format: { with: /^.+@.+\..+$/, message: "Must be a valid email." }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
 end
