bdaaec3eab516a01eeb5f0f59e45cee40ca4464a
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-21 12:33:01 +0200

    defined methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def show
+
+  end
+
+  def create
+    # Is this necessary? There is no new, and the create is done by Devise.
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
