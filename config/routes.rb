2d7b39893905ff97dbead0c12865db59e39b9482
Francois Marie Verdier <francoismarie.verdier@gmail.com>
2018-08-21 11:11:09 +0200

    routes dones => chefs, menus(nested), bookings, reviews(nested), users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, except: [:index, :new, :create]
   devise_for :chefs
+  resources :chefs, except: [:new, :create] do
+    resources :menus, only: [:create, :update, :destroy]
+  end
+  resources :bookings do
+    resources :reviews, only: [:new, :create]
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
