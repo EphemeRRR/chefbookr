d09e1c729752d21188e82ce78c1410e2a1a5f8be
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-21 16:27:18 +0200

    succesful test

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,7 +3,7 @@ class Chef < ApplicationRecord
   has_many :menus
   has_many :bookings, through: :menus, dependent: :destroy
 
-  validates :experience, numericality: { only_integer: true }
+  # validates :experience, numericality: { only_integer: true }
 
   # Devise does this, we think.
   # validates :email, format: { with: /^.+@.+\..+$/, message: "Must be a valid email." }
@@ -12,7 +12,7 @@ class Chef < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   mount_uploader :photo_id, PhotoUploader
 
 end
