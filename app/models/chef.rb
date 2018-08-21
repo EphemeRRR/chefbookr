f636b4c038222670efa610bc456824b2e56dab44
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-21 12:43:22 +0200

    commented-out email validation because Devise does it

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -4,7 +4,10 @@ class Chef < ApplicationRecord
   has_many :bookings, through: :menus, dependent: :destroy
 
   validates :experience, numericality: { only_integer: true }
-  validates :email, format: { with: /^.+@.+\..+$/, message: "Must be a valid email." }
+
+  # Devise does this, we think.
+  # validates :email, format: { with: /^.+@.+\..+$/, message: "Must be a valid email." }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
