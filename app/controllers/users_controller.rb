f9e66771784b0254b60d05e79cfdb93afef8900d
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-21 12:38:35 +0200

    wrote main methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  def show
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  def show
+    # set_user
   end
 
   def create
@@ -8,14 +10,27 @@ class UsersController < ApplicationController
   end
 
   def edit
-
+    # set_user
   end
 
   def update
-
+    @user = User.update(user_params)
+    redirect_to user_path(@user)
   end
 
   def destroy
+    @user.destroy
+    redirect_to root
+  end
 
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name. :last_name, :photo_id,
+                                 :location, :telephone)
   end
 end
