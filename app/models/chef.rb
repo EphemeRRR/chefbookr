a9f18462fbaa588f0af89b953a98d834184356fa
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-21 16:40:28 +0200

    added fields and photo uploader in form

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,7 +3,7 @@ class Chef < ApplicationRecord
   has_many :menus
   has_many :bookings, through: :menus, dependent: :destroy
 
-  # validates :experience, numericality: { only_integer: true }
+  validates :experience, numericality: { only_integer: true, message: "Experience must be a number." }
 
   # Devise does this, we think.
   # validates :email, format: { with: /^.+@.+\..+$/, message: "Must be a valid email." }
