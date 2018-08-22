269247b913c8a969ee9f9a0e34a8f3873a276745
Francois Marie Verdier <francoismarie.verdier@gmail.com>
2018-08-22 12:10:09 +0200

    drop :specialties add specialty to chefs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_162649) do
+ActiveRecord::Schema.define(version: 2018_08_22_094003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_08_20_162649) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "specialty"
     t.index ["email"], name: "index_chefs_on_email", unique: true
     t.index ["reset_password_token"], name: "index_chefs_on_reset_password_token", unique: true
   end
@@ -67,13 +68,6 @@ ActiveRecord::Schema.define(version: 2018_08_20_162649) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
-  end
-
-  create_table "specialties", force: :cascade do |t|
-    t.integer "name"
-    t.string "chef"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
