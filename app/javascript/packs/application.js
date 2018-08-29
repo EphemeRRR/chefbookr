86a8c9ed0d145fbcd0c1870c55559ed3c3225aeb
Christian Rognstad <gudim87@hotmail.com>
2018-08-29 20:14:50 +0200

    made the owl carousell responsive

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,25 @@ $(document).ready(function(){
       loop:true,
       margin:10,
       nav:true,
-      navText : ["<i class='fas fa-chevron-left arrow-carousel-left' ></i>","<i class='fas fa-chevron-right arrow-carousel-right'></i>"]
+      navText : ["<i class='fas fa-chevron-left arrow-carousel-left' ></i>","<i class='fas fa-chevron-right arrow-carousel-right'></i>"],
+      responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
   });
+
 });
 
 chefShowTabs();
